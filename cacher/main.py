99d40ec51a5e36800adb3d108f1d57d75b4a6498
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-01-18 10:35:09 +0100

    Reformating with black

--- cacher/main.py
+++ cacher/main.py
@@ -13,9 +13,10 @@ import datetime
 
 CACHE = None
 try:
-  CACHE = redis.Redis(host=HOST, port=PORT)
+    CACHE = redis.Redis(host=HOST, port=PORT)
 except redis.ConnectionError as e:
-  print(f"Connection error occurred:\n{e}")
+    print(f"Connection error occurred:\n{e}")
+
 
 def add(personId: int, data: dict) -> None:
     """
@@ -26,35 +27,35 @@ def add(personId: int, data: dict) -> None:
     keyData: str = f"{keyBase}data"
     keyTimestamp: str = f"{keyBase}timestamp"
     CACHE.rpush(keyData, serialized)
-    CACHE.rpush(keyTimestamp, int(datetime.datetime.now().timestamp()) )
+    CACHE.rpush(keyTimestamp, int(datetime.datetime.now().timestamp()))
 
 
 async def clean_old(personId: int) -> None:
-  """
+    """
   Asynchronously and periodically remove data older than MAX_SEC
   """
-  # print("Cleaning old")
+    # print("Cleaning old")
 
-  keyBase: str = f"{personId}_"
-  keyData: str = f"{keyBase}data"
-  keyTimestamp: str = f"{keyBase}timestamp"
-  while True:
-    needs_cleaning = True
-    while needs_cleaning:
-      sample: list = CACHE.lrange(keyTimestamp, 0, 0)
-      if len(sample) > 0:
-        toCheck: int = int(float(sample[0]))
-      else:
-          break
-      # print("to Check{:^12}".format(toCheck))
-      clean: bool = toCheck <= int(datetime.datetime.now().timestamp()) - MAX_SEC
-      if clean:
-        print("Cleaning")
-        CACHE.lpop(keyData)
-        CACHE.lpop(keyTimestamp)
-      else:
-        needs_cleaning = False
-    await asyncio.sleep(CLEAR_DELAY)
+    keyBase: str = f"{personId}_"
+    keyData: str = f"{keyBase}data"
+    keyTimestamp: str = f"{keyBase}timestamp"
+    while True:
+        needs_cleaning = True
+        while needs_cleaning:
+            sample: list = CACHE.lrange(keyTimestamp, 0, 0)
+            if len(sample) > 0:
+                toCheck: int = int(float(sample[0]))
+            else:
+                break
+            # print("to Check{:^12}".format(toCheck))
+            clean: bool = toCheck <= int(datetime.datetime.now().timestamp()) - MAX_SEC
+            if clean:
+                print("Cleaning")
+                CACHE.lpop(keyData)
+                CACHE.lpop(keyTimestamp)
+            else:
+                needs_cleaning = False
+        await asyncio.sleep(CLEAR_DELAY)
 
 
 async def pull_data(personId: int) -> None:
@@ -62,34 +63,37 @@ async def pull_data(personId: int) -> None:
   Asynchronously and continuously pull data from server with REQUEST_DELAY delay
   """
     while True:
-      loop = asyncio.get_event_loop()
-      futureResponse = loop.run_in_executor(None, requests.get, f"{BASE_URL}{personId}")
-      response = await futureResponse
-      data: dict    = response.json()
-      trace: dict   = data["trace"]
-      sensors: dict = trace["sensors"]
+        loop = asyncio.get_event_loop()
+        futureResponse = loop.run_in_executor(
+            None, requests.get, f"{BASE_URL}{personId}"
+        )
+        response = await futureResponse
+        data: dict = response.json()
+        trace: dict = data["trace"]
+        sensors: dict = trace["sensors"]
 
-      add(personId, sensors)
-      await asyncio.sleep(REQUEST_DELAY)
+        add(personId, sensors)
+        await asyncio.sleep(REQUEST_DELAY)
 
 
 async def main():
     print(f"execution of loop starts {IDS}")
     try:
-      pullers = asyncio.gather( *(pull_data(i) for i in IDS) )
-      cleaner = asyncio.gather( *(clean_old(i) for i in IDS) )
+        pullers = asyncio.gather(*(pull_data(i) for i in IDS))
+        cleaner = asyncio.gather(*(clean_old(i) for i in IDS))
 
-      all_workers = asyncio.gather(pullers, cleaner)
-      await all_workers
+        all_workers = asyncio.gather(pullers, cleaner)
+        await all_workers
 
-      while True:
-        await asyncio.sleep(10)
+        while True:
+            await asyncio.sleep(10)
     except requests.exceptions.Timeout as e:
-      print(f"Connection to api has timed out:\n{e}")
+        print(f"Connection to api has timed out:\n{e}")
     except requests.exceptions.ConnectionError as e:
-      print(f"There was an error connecting to api:\n{e}")
+        print(f"There was an error connecting to api:\n{e}")
     except redis.ConnectionError as e:
-      print(f"Connection error occurred:\n{e}")
+        print(f"Connection error occurred:\n{e}")
+
 
 logging.basicConfig(level=logging.DEBUG)
 asyncio.run(main(), debug=True)
